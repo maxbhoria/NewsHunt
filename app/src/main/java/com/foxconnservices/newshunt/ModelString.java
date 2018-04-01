3fbe569337cbb5f850aa6144c022a9372d9c7c6d
MOHIT BHORIA <bhoria625@gmail.com>
2018-04-01 18:42:39 +0530

    Initial commit

---
+++ app/src/main/java/com/foxconnservices/newshunt/ModelString.java
@@ -0,0 +1,10 @@
+package com.foxconnservices.newshunt;
+
+public class ModelString{
+
+    public String authorName;
+    public String headline;
+    public String publishedTime;
+    public String newsDetail;
+
+}
